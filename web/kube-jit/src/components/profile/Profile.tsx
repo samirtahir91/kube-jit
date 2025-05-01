7a696da1c2d42fc71c91d87243ff390437a458ea
samirtahir91 <samirtahir91@gmail.com>
2025-05-01 07:33:55 +0100

    nav

--- web/kube-jit/src/components/profile/Profile.tsx
+++ web/kube-jit/src/components/profile/Profile.tsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import './Profile.css'; // Ensure the custom CSS is imported
+import './Profile.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -7,38 +7,51 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import NavBrand from '../navBrand/NavBrand';
 import { UserData } from '../../types';
 import microsoftLogo from '../../assets/azure-icon.svg';
+import { useState } from 'react';
 
 const Profile = ({ user, onSignOut }: { user: UserData; onSignOut: () => void }) => {
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const handleMouseEnter = () => setShowDropdown(true);
+    const handleMouseLeave = () => setShowDropdown(false);
+
     return (
         <div className="py-5">
-            {/* Nav bar with sign in info and links */}
             <Navbar className="navbar" fixed="top" expand="lg">
                 <Container>
                     <Navbar.Brand className="d-flex align-items-center">
-                        <NavBrand /> {/* Main nav brand/logo */}
-                        <Nav.Link href="/" className="text-light ms-3">Home</Nav.Link> {/* Add margin to separate from brand */}
+                        <NavBrand />
+                        <Nav.Link href="/" className="text-light ms-3">Home</Nav.Link>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="custom-navbar-collapse" />
                     <Navbar.Collapse id="custom-navbar-collapse" bsPrefix="custom-navbar-collapse" className="justify-content-end">
-                        <NavDropdown
-                            title={
-                                <>
-                                    <img
-                                        alt=""
-                                        src={user.avatar_url || microsoftLogo} // Use avatar_url if available, otherwise fallback to microsoftLogo
-                                        width="30"
-                                        height="30"
-                                        className="d-inline-block align-top me-2"
-                                    />
-                                    {user.name}
-                                </>
-                            }
+                        <div
                             id="user-dropdown"
-                            align="end"
-                            className="text-light"
+                            onMouseEnter={handleMouseEnter}
+                            onMouseLeave={handleMouseLeave}
+                            className="position-relative"
                         >
-                            <NavDropdown.Item onClick={onSignOut}>Sign Out</NavDropdown.Item>
-                        </NavDropdown>
+                            <NavDropdown
+                                title={
+                                    <>
+                                        <img
+                                            alt=""
+                                            src={user.avatar_url || microsoftLogo}
+                                            width="30"
+                                            height="30"
+                                            className="d-inline-block align-top me-2"
+                                        />
+                                        {user.name}
+                                    </>
+                                }
+                                id="user-dropdown-toggle"
+                                align="end"
+                                className="text-light"
+                                show={showDropdown}
+                            >
+                                <NavDropdown.Item onClick={onSignOut}>Sign Out</NavDropdown.Item>
+                            </NavDropdown>
+                        </div>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
