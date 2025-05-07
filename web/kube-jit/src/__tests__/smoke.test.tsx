1ebbf67f7c9c8a10a01686b6182300526995d4a2
samirtahir91 <samirtahir91@gmail.com>
2025-05-07 19:09:08 +0100

    test-1

--- web/kube-jit/src/__tests__/smoke.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import { screen } from "@testing-library/dom";
-import "@testing-library/jest-dom";
-describe("smoke test", () => {
-  it("renders hello world", () => {
-    render(<div>Hello World</div>);
-    expect(screen.getByText("Hello World")).toBeInTheDocument();
-  });
-});
