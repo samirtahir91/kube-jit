c37de98541eb7c6e841b0ce82b2dcd8e71e5983d
samirtahir91 <samirtahir91@gmail.com>
2025-05-09 17:00:08 +0100

    working-init

--- web/kube-jit/vite.config.ts
+++ web/kube-jit/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vitest/config'; // <-- change from 'vite' to 'vitest/config'
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
-    setupFiles: './src/vitest.setup.ts'
-  }
+    setupFiles: './src/setupTests.ts',
+    globals: true,
+  },
 });
