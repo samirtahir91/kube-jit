0b542df5926c8cf33f55637423d704e539953ea0
samirtahir91 <samirtahir91@gmail.com>
2025-05-10 08:12:15 +0100

    app

--- web/kube-jit/src/components/inputTag/InputTag.test.tsx
+++ web/kube-jit/src/components/inputTag/InputTag.test.tsx
@@ -1,4 +1,4 @@
-import React, { createRef } from 'react';
+import { createRef } from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, it, expect, vi } from 'vitest';
 import InputTag from './InputTag';
