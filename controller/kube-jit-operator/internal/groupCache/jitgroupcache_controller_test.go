f577431fbcf3a70032b7aa672bd547fb3d1ad522
samirtahir91 <samirtahir91@gmail.com>
2025-05-14 16:55:34 +0100

    jitcache-test

--- controller/kube-jit-operator/internal/groupCache/jitgroupcache_controller_test.go
+++ controller/kube-jit-operator/internal/groupCache/jitgroupcache_controller_test.go
@@ -28,9 +28,13 @@ import (
 	"kube-jit-operator/test/utils"
 	"os/exec"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 const (
@@ -48,7 +52,7 @@ func init() {
 	}
 }
 
-var _ = Describe("JustInTimeConfig Controller", Ordered, Label("integration"), func() {
+var _ = Describe("JitGroupCache Controller", Ordered, Label("integration"), func() {
 
 	BeforeAll(func() {
 		By("removing manager config")
@@ -96,6 +100,143 @@ var _ = Describe("JustInTimeConfig Controller", Ordered, Label("integration"), f
 			By("Creating the operator KubeJitConfig")
 			err := utils.CreateJitConfig(ctx, k8sClient, ValidClusterRole, TestNamespace)
 			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+	Context("When reconciling JitGroupCache", func() {
+		const (
+			testGroupID   = "group-123"
+			testGroupName = "Test Group"
+		)
+
+		BeforeEach(func() {
+			// Clean up before each test
+			_, _ = utils.Run(exec.Command("kubectl", "delete", "ns", TestNamespace))
+		})
+
+		AfterEach(func() {
+			_, _ = utils.Run(exec.Command("kubectl", "delete", "ns", TestNamespace))
+		})
+
+		It("should create JitGroupCache if it does not exist", func() {
+			By("Deleting the JitGroupCache CR if it exists")
+			_, _ = utils.Run(exec.Command("kubectl", "delete", "jitgroupcache", "jitgroupcache"))
+
+			By("Triggering a reconcile by creating a labeled namespace")
+			ns := &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: TestNamespace,
+					Labels: map[string]string{
+						"jit.kubejit.io/adopt": "true",
+					},
+					Annotations: map[string]string{
+						"jit.kubejit.io/group_id":   testGroupID,
+						"jit.kubejit.io/group_name": testGroupName,
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, ns)).To(Succeed())
+
+			By("Eventually the JitGroupCache CR should exist and contain the group")
+			Eventually(func(g Gomega) {
+				cache := &jitv1.JitGroupCache{}
+				err := k8sClient.Get(ctx, types.NamespacedName{Name: "jitgroupcache"}, cache)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(cache.Spec.Groups).To(ContainElement(jitv1.JitGroup{
+					Namespace: TestNamespace,
+					GroupID:   testGroupID,
+					GroupName: testGroupName,
+				}))
+			}, "20s", "1s").Should(Succeed())
+		})
+
+		It("should update JitGroupCache when a namespace is deleted", func() {
+			By("Creating a labeled namespace")
+			ns := &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: TestNamespace,
+					Labels: map[string]string{
+						"jit.kubejit.io/adopt": "true",
+					},
+					Annotations: map[string]string{
+						"jit.kubejit.io/group_id":   testGroupID,
+						"jit.kubejit.io/group_name": testGroupName,
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, ns)).To(Succeed())
+
+			By("Ensuring the group is present in the cache")
+			Eventually(func(g Gomega) {
+				cache := &jitv1.JitGroupCache{}
+				err := k8sClient.Get(ctx, types.NamespacedName{Name: "jitgroupcache"}, cache)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(cache.Spec.Groups).To(ContainElement(jitv1.JitGroup{
+					Namespace: TestNamespace,
+					GroupID:   testGroupID,
+					GroupName: testGroupName,
+				}))
+			}, "20s", "1s").Should(Succeed())
+
+			By("Deleting the namespace")
+			Expect(k8sClient.Delete(ctx, ns)).To(Succeed())
+
+			By("Eventually the group should be removed from the cache")
+			Eventually(func(g Gomega) {
+				cache := &jitv1.JitGroupCache{}
+				err := k8sClient.Get(ctx, types.NamespacedName{Name: "jitgroupcache"}, cache)
+				g.Expect(err).NotTo(HaveOccurred())
+				for _, group := range cache.Spec.Groups {
+					g.Expect(group.Namespace).NotTo(Equal(TestNamespace))
+				}
+			}, "20s", "1s").Should(Succeed())
+		})
+
+		It("should update JitGroupCache when a namespace's group info changes", func() {
+			By("Creating a labeled namespace")
+			ns := &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: TestNamespace,
+					Labels: map[string]string{
+						"jit.kubejit.io/adopt": "true",
+					},
+					Annotations: map[string]string{
+						"jit.kubejit.io/group_id":   testGroupID,
+						"jit.kubejit.io/group_name": testGroupName,
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, ns)).To(Succeed())
+
+			By("Ensuring the group is present in the cache")
+			Eventually(func(g Gomega) {
+				cache := &jitv1.JitGroupCache{}
+				err := k8sClient.Get(ctx, types.NamespacedName{Name: "jitgroupcache"}, cache)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(cache.Spec.Groups).To(ContainElement(jitv1.JitGroup{
+					Namespace: TestNamespace,
+					GroupID:   testGroupID,
+					GroupName: testGroupName,
+				}))
+			}, "20s", "1s").Should(Succeed())
+
+			By("Updating the namespace's group info")
+			patch := client.MergeFrom(ns.DeepCopy())
+			ns.Annotations["jit.kubejit.io/group_id"] = "group-456"
+			ns.Annotations["jit.kubejit.io/group_name"] = "New Group"
+			Expect(k8sClient.Patch(ctx, ns, patch)).To(Succeed())
+
+			By("Eventually the cache should reflect the new group info")
+			Eventually(func(g Gomega) {
+				cache := &jitv1.JitGroupCache{}
+				err := k8sClient.Get(ctx, types.NamespacedName{Name: "jitgroupcache"}, cache)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(cache.Spec.Groups).To(ContainElement(jitv1.JitGroup{
+					Namespace: TestNamespace,
+					GroupID:   "group-456",
+					GroupName: "New Group",
+				}))
+			}, "20s", "1s").Should(Succeed())
 		})
 	})
 })
